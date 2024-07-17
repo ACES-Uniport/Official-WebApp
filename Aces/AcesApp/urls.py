d08b871a78b72943fcf3c4dc13f6e67d0ccb18f5
Nworah-Gabriel <gabrielnworah6@gmail.com>
2024-07-17 00:13:30 +0000

    added a conditional statement to the urls.py

--- Aces/AcesApp/urls.py
+++ Aces/AcesApp/urls.py
@@ -4,7 +4,11 @@
 from django.contrib import admin
 from django.urls import path
 import AcesApp.views as views
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('', views.home, name="home"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
